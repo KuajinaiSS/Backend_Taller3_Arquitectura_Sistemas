87c297a1f58106013336c28a4d9b0209a478f23a
KuajinaiSS <kuajinaiss@gmail.com>
2025-01-06 22:43:07 -0300

    feature: updated the workflow

--- config/database.js
+++ config/database.js
@@ -8,6 +8,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
   },
   test: {
@@ -18,10 +19,11 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql"
   },
 };
